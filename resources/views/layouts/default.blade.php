dc783d52099917270630cdafa2f5a92df689ce97
lxz2133 <lxz2133@gmail.com>
2018-07-11 08:33:12 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lxz
+ * Date: 2018/7/11
+ * Time: 15:59
+ */
+?><!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Sample')</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
